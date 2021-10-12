7f7e22d836cf8b502c0d8ccfde37018cebc78970
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-10-12 16:23:48 +0200

    PM / sleep: Fix compilation when SEC_DEBUG is disabled
    Change-Id: Iea05c1062688759c20f0c6b3077b3a4d0e5c4530
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -40,6 +40,7 @@ static int try_to_freeze_tasks(bool user_only)
 	unsigned int elapsed_msecs;
 	bool wakeup = false;
 	int sleep_usecs = USEC_PER_MSEC;
+#ifdef CONFIG_SEC_DEBUG
 	char *sys_state[SYSTEM_END] = {
 		"BOOTING",
 		"SCHEDULING",
@@ -48,6 +49,7 @@ static int try_to_freeze_tasks(bool user_only)
 		"POWER_OFF",
 		"RESTART",
 	};
+#endif
 
 	start = ktime_get_boottime();
 
@@ -58,8 +60,10 @@ static int try_to_freeze_tasks(bool user_only)
 	if (!user_only)
 		freeze_workqueues_begin();
 
+#ifdef CONFIG_SEC_DEBUG
 	sec_debug_set_unfrozen_task((uint64_t)NULL);
 	sec_debug_set_unfrozen_task_count((uint64_t)0);
+#endif
 
 	while (true) {
 		todo = 0;
@@ -70,10 +74,14 @@ static int try_to_freeze_tasks(bool user_only)
 
 			if (!freezer_should_skip(p)) {
 				todo++;
+#ifdef CONFIG_SEC_DEBUG
 				sec_debug_set_unfrozen_task((uint64_t)p);
+#endif
 			}
 		}
+#ifdef CONFIG_SEC_DEBUG
 		sec_debug_set_unfrozen_task_count((uint64_t)todo);
+#endif
 
 		read_unlock(&tasklist_lock);
 
@@ -123,13 +131,17 @@ static int try_to_freeze_tasks(bool user_only)
 			if (p != current && !freezer_should_skip(p)
 			    && freezing(p) && !frozen(p)) {
 				sched_show_task(p);
+#ifdef CONFIG_SEC_DEBUG
 				sec_debug_set_extra_info_backtrace_task(p);
 				sec_debug_set_extra_info_unfz(p->comm);
+#endif
 			}
 		}
 		read_unlock(&tasklist_lock);
 
+#ifdef CONFIG_SEC_DEBUG
 		sec_debug_set_extra_info_unfz(sys_state[system_state]);
+#endif
 #if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
 		panic("fail to freeze tasks");
 #endif
@@ -140,8 +152,10 @@ static int try_to_freeze_tasks(bool user_only)
 
 	s3c2410wdt_emergency_multistage_wdt_start();
 
+#ifdef CONFIG_SEC_DEBUG
 	sec_debug_set_unfrozen_task((uint64_t)NULL);
 	sec_debug_set_unfrozen_task_count((uint64_t)0);
+#endif
 
 	return todo ? -EBUSY : 0;
 }
