BayLibre/android_kernel_common

ANDROID: Incremental fs: Add zstd compression support
Bug: 160634783
Test: incfs_test passes
Signed-off-by: Paul Lawrence <paullawrence@google.com>
Change-Id: Iba28b535d2d5183859ffc721204b036434132d9b

fs/incfs/data_mgmt.h
@@ -13,6 +13,7 @@
 #include <linux/rcupdate.h>
 #include <linux/completion.h>
 #include <linux/wait.h>
+#include <linux/zstd.h>
 #include <crypto/hash.h>
 #include <linux/rwsem.h>
 
@@ -166,6 +167,12 @@ struct mount_info {
 	spinlock_t mi_per_uid_read_timeouts_lock;
 	struct incfs_per_uid_read_timeouts *mi_per_uid_read_timeouts;
 	int mi_per_uid_read_timeouts_size;
+
+	/* zstd workspace */
+	struct mutex mi_zstd_workspace_mutex;
+	void *mi_zstd_workspace;
+	ZSTD_DStream *mi_zstd_stream;
+	struct delayed_work mi_zstd_workspace_work;
 };
 
 struct data_file_block {

UtsavBalar1231/kernel_samsung_universal9611

fs/incfs/data_mgmt.h
@@ -13,6 +13,7 @@
 #include <linux/rcupdate.h>
 #include <linux/completion.h>
 #include <linux/wait.h>
+#include <linux/zstd.h>
 #include <crypto/hash.h>
 #include <linux/rwsem.h>
 
@@ -166,6 +167,12 @@ struct mount_info {
 	spinlock_t mi_per_uid_read_timeouts_lock;
 	struct incfs_per_uid_read_timeouts *mi_per_uid_read_timeouts;
 	int mi_per_uid_read_timeouts_size;
+
+	/* zstd workspace */
+	struct mutex mi_zstd_workspace_mutex;
+	void *mi_zstd_workspace;
+	ZSTD_DStream *mi_zstd_stream;
+	struct delayed_work mi_zstd_cleanup_work;
 };
 
 struct data_file_block {